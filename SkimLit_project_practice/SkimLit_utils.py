ff4af302b2b20ada3244dd485d06af3569b38fae
Zongyuan Wang <72905015+ZYWZong@users.noreply.github.com>
2024-12-23 17:30:28 -0800

    Update SkimLit_utils.py

--- SkimLit_project_practice/SkimLit_utils.py
+++ SkimLit_project_practice/SkimLit_utils.py
@@ -1,7 +1,7 @@
 """
 This file contains helper functions for the practice project SkimLit
 
-Acknowledgement: 
+Acknowledgment: 
   Parts of the functions are adopted from "TensorFlow for Deep Learning Bootcamp" taught by Daniel Bourke on Udemy.
 """
 
